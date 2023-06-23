f2c26c32b6b9af796c5667a2812cbec789ac735e
rkamika <45544468+rkamika@users.noreply.github.com>
2023-06-23 09:47:30 -0300

    Adicionar arquivos do site

--- app.py
+++ app.py
@@ -6,10 +6,30 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/calcular', methods=['POST'])
+@app.route('/calcular', methods=['GET', 'POST'])
 def calcular():
-    cheques = []
-    for i in range(1, 6):  # Considerando até 5 cheques, você pode ajustar esse valor conforme necessário
-        if f'valor_cheque_{i}' in request.form:
-            valor_cheque = float(request.form[f'valor_cheque_{i}'])
-            data = request.form[f'data_{
+    if request.method == 'POST':
+        cheques = []
+        for i in range(1, 6):  # Considerando até 5 cheques, você pode ajustar esse valor conforme necessário
+            if f'valor_cheque_{i}' in request.form:
+                valor_cheque = float(request.form[f'valor_cheque_{i}'])
+                data = request.form[f'data_{i}']
+                desconto = calcular_desconto(valor_cheque)
+                valor_liquido = valor_cheque - desconto
+                cheques.append({'valor': valor_cheque, 'data': data, 'desconto': desconto, 'valor_liquido': valor_liquido})
+        
+        valor_total_cheques = sum(cheque['valor'] for cheque in cheques)
+        total_desconto = sum(cheque['desconto'] for cheque in cheques)
+        total_descontado = sum(cheque['valor_liquido'] for cheque in cheques)
+        
+        return render_template('resultado.html', cheques=cheques, valor_total_cheques=valor_total_cheques, total_desconto=total_desconto, total_descontado=total_descontado)
+    else:
+        return render_template('index.html')
+
+def calcular_desconto(valor_cheque):
+    taxa_juros = float(request.form['taxa_juros'])
+    desconto = valor_cheque * (taxa_juros / 100)
+    return desconto
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-import datetime
 
 app = Flask(__name__)
 
@@ -7,35 +6,30 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/calcular', methods=['POST'])
+@app.route('/calcular', methods=['GET', 'POST'])
 def calcular():
-    cheques = []
-    for i in range(1, 6):  # Considerando até 5 cheques, você pode ajustar esse valor conforme necessário
-        if f'valor_cheque_{i}' in request.form:
-            valor_cheque = float(request.form[f'valor_cheque_{i}'])
-            data = request.form[f'data_{i}']
-            cheques.append((valor_cheque, data))
+    if request.method == 'POST':
+        cheques = []
+        for i in range(1, 6):  # Considerando até 5 cheques, você pode ajustar esse valor conforme necessário
+            if f'valor_cheque_{i}' in request.form:
+                valor_cheque = float(request.form[f'valor_cheque_{i}'])
+                data = request.form[f'data_{i}']
+                desconto = calcular_desconto(valor_cheque)
+                valor_liquido = valor_cheque - desconto
+                cheques.append({'valor': valor_cheque, 'data': data, 'desconto': desconto, 'valor_liquido': valor_liquido})
+        
+        valor_total_cheques = sum(cheque['valor'] for cheque in cheques)
+        total_desconto = sum(cheque['desconto'] for cheque in cheques)
+        total_descontado = sum(cheque['valor_liquido'] for cheque in cheques)
+        
+        return render_template('resultado.html', cheques=cheques, valor_total_cheques=valor_total_cheques, total_desconto=total_desconto, total_descontado=total_descontado)
+    else:
+        return render_template('index.html')
 
-    total_cheques = len(cheques)
-    total_valor_cheques = sum(cheque[0] for cheque in cheques)
-
-    desconto_total = 0
-    for cheque in cheques:
-        taxa_juros = float(request.form['taxa_juros'])
-        valor_cheque = cheque[0]
-        data = cheque[1]
-
-        # Convertendo a taxa de juros para decimal
-        taxa_decimal = taxa_juros / 100
-
-        # Calculando o desconto
-        desconto = taxa_decimal * valor_cheque
-        desconto_total += desconto
-
-    valor_total_descontado = total_valor_cheques - desconto_total
-
-    return render_template('resultado.html', total_cheques=total_cheques, total_valor_cheques=total_valor_cheques,
-                           desconto_total=desconto_total, valor_total_descontado=valor_total_descontado)
+def calcular_desconto(valor_cheque):
+    taxa_juros = float(request.form['taxa_juros'])
+    desconto = valor_cheque * (taxa_juros / 100)
+    return desconto
 
 if __name__ == '__main__':
     app.run(debug=True)
